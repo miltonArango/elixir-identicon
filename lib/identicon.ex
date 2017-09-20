db1765c73eca00731f1cdf8f08304abeb4f2c6e2
Milton Arango <milton.arango@codescrum.com>
2017-09-20 17:37:08 -0500

    Section 6, Lecture 42

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -16,6 +16,7 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
   end
 
   def hash_input(input) do
@@ -25,9 +26,13 @@ defmodule Identicon do
     %Identicon.Image{hex: hex}
   end
 
-  def pick_color(image) do
-    %Identicon.Image{hex: [r, g, b | _tail]} = image
-    [r, g ,b]
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk(3)
   end
 
 end
